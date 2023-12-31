f50f59f0d13abb6aeb44a11af2b1676acc302ad2
dimasicr <dimas.icr@gmail.com>
2023-12-31 13:17:18 +0700

    add swipe control

--- main_v2.js
+++ main_v2.js
@@ -5,6 +5,70 @@ const selectPadding = document.getElementById('paddings');
 const selectThemes = document.getElementById('themes');
 const selectThreshold = document.getElementById('recursion_threshold');
 
+let touchStartX = 0;
+let touchEndX = 0;
+let currentPattern = selectBox.value;
+
+// Function to handle swipe
+function handleSwipe() {
+    const swipeThreshold = 50; // Adjust this value as needed
+
+    // Calculate the distance of swipe
+    const swipeLength = touchEndX - touchStartX;
+
+    if (swipeLength > swipeThreshold) {
+        // Swipe right
+        console.log("Swiped right!");
+        if (currentPattern === 'symmetric') {
+          currentPattern = 'triplet';
+          selectBox.value = 'triplet';
+        } 
+        else if (currentPattern === 'triplet') {
+          currentPattern = 'asymmetric';
+          selectBox.value = 'asymmetric';
+        } 
+        else if (currentPattern === 'asymmetric') {
+          currentPattern = 'symmetric';
+          selectBox.value = 'symmetric';
+        }
+
+    } else if (swipeLength < -swipeThreshold) {
+        // Swipe left
+        console.log("Swiped left!");
+        if (currentPattern === 'symmetric') {
+          currentPattern = 'asymmetric';
+          selectBox.value = 'asymmetric';
+        } 
+        else if (currentPattern === 'asymmetric') {
+          currentPattern = 'triplet';
+          selectBox.value = 'triplet';
+        } 
+        else if (currentPattern === 'triplet') {
+          currentPattern = 'symmetric';
+          selectBox.value = 'symmetric';
+        } 
+    }
+    redraw();
+}
+
+svg.addEventListener("touchstart", (event) => {
+    touchStartX = event.touches[0].clientX;
+});
+
+svg.addEventListener("touchend", (event) => {
+    touchEndX = event.changedTouches[0].clientX;
+    handleSwipe();
+});
+
+// Mouse events (for desktop)
+svg.addEventListener("mousedown", (event) => {
+    touchStartX = event.clientX;
+});
+
+svg.addEventListener("mouseup", (event) => {
+    touchEndX = event.clientX;
+    handleSwipe();
+});
 
 function redraw(){
   setup(400, 400, themes[selectThemes.value].secondaryColor);
