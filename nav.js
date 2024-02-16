4f76185e5cb774b69622dbc0df70c9b25f9527e7
dimasicr <dimas.icr@gmail.com>
2024-02-17 05:20:39 +0700

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,28 +1,26 @@
 document.write('<h2>Gallery</h2>');
-document.write('
-<ul class="nav-links">
-  <li><a href="index.html">Apollonian Kawung</a></li>
-  <li><a href="fibonacci.html">Fibonacci Kawung</a></li>
-  <li><a href="poincare.html">Poincare Kawung</a></li>
-  <li><a href="tumpal.html">Sierpinski Tumpal</a></li>
-  <li><a href="semiregular.html">Tesselation of Kawung </a></li>
-  <li><a href="concentric.html"> Concentric Kawung </a></li>
-  <li><a href="parametricbuketan.html"> Parametric Buketan </a></li>
-  <li><a href="tumpaltesselation.html"> Tesselation of Tumpal </a></li>
-</ul>')
+document.write(
+  '<ul class="nav-links">' +
+    '<li><a href="index.html">Apollonian Kawung</a></li>' +
+    '<li><a href="fibonacci.html">Fibonacci Kawung</a></li>' +
+    '<li><a href="poincare.html">Poincare Kawung</a></li>' +
+    '<li><a href="tumpal.html">Sierpinski Tumpal</a></li>' +
+    '<li><a href="semiregular.html">Tesselation of Kawung</a></li>' +
+    '<li><a href="concentric.html">Concentric Kawung</a></li>' +
+    '<li><a href="parametricbuketan.html">Parametric Buketan</a></li>' +
+    '<li><a href="tumpaltesselation.html">Tesselation of Tumpal</a></li>' +
+  '</ul>'
+);
+
 document.write('<h2>Tutorial</h2>');
-document.write('
-<ul class="nav-links">
-  <li><a href="circular.html"> Circular Kawung</a></li>
-  <li><a href="apolloniangasket.html"> Apollonian Kawung</a></li>
-  <li><a href="nleaf.html"> N Leaf Kawung</a></li>
-  <li><a href="threeleaves.html"> Three Leaves Kawung</a></li>
-  <li><a href="sixleaves.html"> Six Leaves Kawung</a></li>
-  <li><a href="eightleaves.html"> Eight Leaves Kawung</a></li>
-  <li><a href="twelveleaves.html"> Twelve Leaves Kawung</a></li>
-</ul>');
-
-
-
-
-
+document.write(
+  '<ul class="nav-links">' +
+    '<li><a href="circular.html">Circular Kawung</a></li>' +
+    '<li><a href="apolloniangasket.html">Apollonian Kawung</a></li>' +
+    '<li><a href="nleaf.html">N Leaf Kawung</a></li>' +
+    '<li><a href="threeleaves.html">Three Leaves Kawung</a></li>' +
+    '<li><a href="sixleaves.html">Six Leaves Kawung</a></li>' +
+    '<li><a href="eightleaves.html">Eight Leaves Kawung</a></li>' +
+    '<li><a href="twelveleaves.html">Twelve Leaves Kawung</a></li>' +
+  '</ul>'
+);
