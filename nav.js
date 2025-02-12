d9d1fce1b326986acef63546a6b0f9e295021060
dimasicr <dimas.icr@gmail.com>
2025-02-12 18:57:10 +0700

    Update nav.js

--- nav.js
+++ nav.js
@@ -12,7 +12,6 @@ document.write(
     '<li><a href="/fibonacci.html">Fibonacci Kawung</a></li>' +
     '<li><a href="/poincare.html">Poincare Kawung</a></li>' +
     '<li><a href="/semiregular.html">Tesselation of Kawung</a></li>' +
-    '<li><a href="/hyperbolic.html">Hyperbolic Kawung</a></li>' +
     '<li><a href="/parametricbuketan.html">Parametric Buketan</a></li>' +
     '<li><a href="/tumpaltesselation.html">Tesselation of Tumpal</a></li>' +
     '<li><a href="/starrynight.html"> Starry Night </a></li>' +
@@ -26,9 +25,6 @@ document.write(
     '<li><a href="/circular.html">Circular Kawung</a></li>' +
     '<li><a href="/apolloniangasket.html">Apollonian Kawung</a></li>' +
     '<li><a href="/nleaf.html">N Leaf Kawung</a></li>' +
-    '<li><a href="/threeleaves.html">Three Leaves Kawung</a></li>' +
-    '<li><a href="/sixleaves.html">Six Leaves Kawung</a></li>' +
-    '<li><a href="/eightleaves.html">Eight Leaves Kawung</a></li>' +
     '<li><a href="/twelveleaves.html">Twelve Leaves Kawung</a></li>' +
   '</ul>'
 );
