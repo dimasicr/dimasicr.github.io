ca287feb060f50086e409ab3cfe654b9c859982f
dimasicr <dimas.icr@gmail.com>
2025-02-12 14:24:15 +0700

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,36 +1,34 @@
 document.write('<h2>App</h2>');
 document.write(
   '<ul class="nav-links">' +
-    '<li><a href="app/batiktumpalmaker.html"> Batik Tumpal Maker </a></li>' +
-    '<li><a href="tumpalfractal.html"> Fractal of Tumpal </a></li>' +
+    '<li><a href="/app/batiktumpalmaker.html"> Batik Tumpal Maker </a></li>'
   '</ul>'
 );
 
 document.write('<h2>Gallery</h2>');
 document.write(
   '<ul class="nav-links">' +
-    '<li><a href="index.html">Apollonian Kawung</a></li>' +
-    '<li><a href="fibonacci.html">Fibonacci Kawung</a></li>' +
-    '<li><a href="poincare.html">Poincare Kawung</a></li>' +
-    // '<li><a href="tumpal.html">Sierpinski Tumpal</a></li>' +
-    '<li><a href="semiregular.html">Tesselation of Kawung</a></li>' +
-    '<li><a href="hyperbolic.html">Hyperbolic Kawung</a></li>' +
-    '<li><a href="parametricbuketan.html">Parametric Buketan</a></li>' +
-    '<li><a href="tumpaltesselation.html">Tesselation of Tumpal</a></li>' +
-    '<li><a href="starrynight.html"> Starry Night </a></li>' +
-    '<li><a href="tumpalfractal.html"> Fractal of Tumpal </a></li>' +
+    '<li><a href="/index.html">Apollonian Kawung</a></li>' +
+    '<li><a href="/fibonacci.html">Fibonacci Kawung</a></li>' +
+    '<li><a href="/poincare.html">Poincare Kawung</a></li>' +
+    '<li><a href="/semiregular.html">Tesselation of Kawung</a></li>' +
+    '<li><a href="/hyperbolic.html">Hyperbolic Kawung</a></li>' +
+    '<li><a href="/parametricbuketan.html">Parametric Buketan</a></li>' +
+    '<li><a href="/tumpaltesselation.html">Tesselation of Tumpal</a></li>' +
+    '<li><a href="/starrynight.html"> Starry Night </a></li>' +
+    '<li><a href="/tumpalfractal.html"> Fractal of Tumpal </a></li>' +
   '</ul>'
 );
 
 document.write('<h2>Tutorial</h2>');
 document.write(
   '<ul class="nav-links">' +
-    '<li><a href="circular.html">Circular Kawung</a></li>' +
-    '<li><a href="apolloniangasket.html">Apollonian Kawung</a></li>' +
-    '<li><a href="nleaf.html">N Leaf Kawung</a></li>' +
-    '<li><a href="threeleaves.html">Three Leaves Kawung</a></li>' +
-    '<li><a href="sixleaves.html">Six Leaves Kawung</a></li>' +
-    '<li><a href="eightleaves.html">Eight Leaves Kawung</a></li>' +
-    '<li><a href="twelveleaves.html">Twelve Leaves Kawung</a></li>' +
+    '<li><a href="/circular.html">Circular Kawung</a></li>' +
+    '<li><a href="/apolloniangasket.html">Apollonian Kawung</a></li>' +
+    '<li><a href="/nleaf.html">N Leaf Kawung</a></li>' +
+    '<li><a href="/threeleaves.html">Three Leaves Kawung</a></li>' +
+    '<li><a href="/sixleaves.html">Six Leaves Kawung</a></li>' +
+    '<li><a href="/eightleaves.html">Eight Leaves Kawung</a></li>' +
+    '<li><a href="/twelveleaves.html">Twelve Leaves Kawung</a></li>' +
   '</ul>'
 );
